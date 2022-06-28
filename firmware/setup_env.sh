2e2ab33d7419d9ca5f42117663a4f91e9674d8d1
Dan Sciortino <disco@slac.stanford.edu>
2022-06-28 10:28:50 -0700

    Setting proj structure

---
+++ firmware/setup_env.sh
@@ -0,0 +1,5 @@
+# Setup the Xilinx software and licensing
+source /afs/slac/g/reseng/xilinx/vivado_2021.2/Vivado/2021.2/settings64.sh
+
+# Setup VCS
+source  /afs/slac.stanford.edu/g/reseng/synopsys/vcs/R-2020.12-1/settings.sh
