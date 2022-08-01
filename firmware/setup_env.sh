b2e4fcd1e8754b41177cad28eef621e06f700e37
Dan Sciortino <disco@slac.stanford.edu>
2022-08-01 16:39:49 -0700

    more setup

--- firmware/setup_env.sh
+++ firmware/setup_env.sh
@@ -2,4 +2,4 @@
 source /afs/slac/g/reseng/xilinx/vivado_2021.2/Vivado/2021.2/settings64.sh
 
 # Setup VCS
-source  /afs/slac.stanford.edu/g/reseng/synopsys/vcs/R-2020.12-1/settings.sh
+source  /afs/slac.stanford.edu/g/reseng/synopsys/vcs/S-2021.09-SP1-1/settings.sh
