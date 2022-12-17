e7f0327285b3fa509609eb298de9377280c2aa2e
Dan Sciortino <disco@slac.stanford.edu>
2022-12-16 16:22:02 -0800

    bumped vivado version

--- firmware/setup_env.sh
+++ firmware/setup_env.sh
@@ -1,5 +1,7 @@
 # Setup the Xilinx software and licensing
-source /afs/slac/g/reseng/xilinx/vivado_2021.2/Vivado/2021.2/settings64.sh
 
-# Setup VCS
-source  /afs/slac.stanford.edu/g/reseng/synopsys/vcs/S-2021.09-SP1-1/settings.sh
+source /afs/slac.stanford.edu/g/reseng/xilinx/vivado_2022.1/Vivado/2022.1/settings64.sh
+# Setup the VCS software
+source  /afs/slac/g/reseng/vol33/synopsys/vcs/R-2020.12-1/settings.sh
+
+source /afs/slac.stanford.edu/g/reseng/zeromq/4.2.0/settings.sh
