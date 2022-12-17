adafac5090e6a7f3790f72d3b50caa9cad044b5c
Dan Sciortino <disco@slac.stanford.edu>
2022-12-16 16:26:01 -0800

    work on software path and rogue version

--- software/script/setupLibPaths.py
+++ software/script/setupLibPaths.py
@@ -6,4 +6,4 @@ top_level = os.path.realpath(__file__).split('software')[0]
 pr.addLibraryPath(top_level + 'firmware/submodules/axi-pcie-core/python')
 pr.addLibraryPath(top_level + 'firmware/submodules/surf/python')
 pr.addLibraryPath(top_level + 'firmware/python')
-pr.addLibraryPath(top_level + 'software/python')
+# pr.addLibraryPath(top_level + 'software/python')
