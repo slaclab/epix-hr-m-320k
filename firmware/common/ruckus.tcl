f1f64aa7c10ac05c3dd8e847d765815b7095e95c
Dan Sciortino <disco@slac.stanford.edu>
2023-02-07 11:21:01 -0800

    updating ruckus files for new repo

--- firmware/common/ruckus.tcl
+++ firmware/common/ruckus.tcl
@@ -1,7 +1,7 @@
 # Load RUCKUS library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
-# loadSource -lib epix_hr_core -dir "$::DIR_PATH/epix-hr-core"
+loadSource -lib epix_hr_leap_common -dir "$::DIR_PATH/epix-hr-leap_common"
 
 # Load application
 loadSource -dir  "$::DIR_PATH/application"
