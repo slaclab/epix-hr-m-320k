cc68997953bd80ecd7d28d80c978e70325312dcc
Dawood Alnajjar <dnajjar@slac.stanford.edu>
2023-07-31 16:40:52 -0700

    Fixed create script

--- software/notebook/createEnv.sh
+++ software/notebook/createEnv.sh
@@ -1,1 +1,6 @@
+##################################
+# Setup environment
+##################################
+source /afs/slac/g/reseng/rogue/anaconda_new_a/anaconda3/etc/profile.d/conda.sh
+
 conda env create --file=env.yml
