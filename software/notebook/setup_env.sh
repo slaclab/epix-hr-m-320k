180aec714bd75e9728c82d06099311a647a6aad9
Dawood Alnajjar <dnajjar@slac.stanford.edu>
2023-07-31 16:39:25 -0700

    Adding setup env script.

---
+++ software/notebook/setup_env.sh
@@ -0,0 +1,11 @@
+
+##################################
+# Setup environment
+##################################
+source /afs/slac/g/reseng/rogue/anaconda_new_a/anaconda3/etc/profile.d/conda.sh
+
+##################################
+# Activate Rogue conda Environment
+##################################
+conda activate ePixHrM
+
