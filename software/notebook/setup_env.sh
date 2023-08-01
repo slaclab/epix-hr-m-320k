0e1e90cb2f412385cbaa3d20ec844c6ff825c3a3
Dawood Alnajjar <dnajjar@slac.stanford.edu>
2023-07-31 19:08:08 -0700

    Make env point to Dawood's home folder.

--- software/notebook/setup_env.sh
+++ software/notebook/setup_env.sh
@@ -7,5 +7,5 @@ source /afs/slac/g/reseng/rogue/anaconda_new_a/anaconda3/etc/profile.d/conda.sh
 ##################################
 # Activate Rogue conda Environment
 ##################################
-conda activate ePixHrM
+conda activate /afs/slac/g/controls/development/users/dnajjar/.conda/envs/ePixHrM
 
