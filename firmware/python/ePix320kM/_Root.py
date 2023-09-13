111962b51a548d7184b835fc5552a70a0a58ff4c
Dawood Alnajjar <dnajjar@slac.stanford.edu>
2023-09-13 11:36:27 -0700

    Added some boundary checks in receiver.

--- firmware/python/ePix320kM/_Root.py
+++ firmware/python/ePix320kM/_Root.py
@@ -38,10 +38,13 @@ class fullRateDataReceiver(ePixHrMv2.DataReceiverEpixHrMv2):
         self.currentFrameCount = 0
 
     def process(self,frame):
-        super().process(frame)
+        if (self.currentFrameCount >= 1000) :
+            print("Max acquistion size of fullRateDataReceiver of 1000 reached. Cleanup dataDebug. Discarding new data.")
+        else :
+            super().process(frame)
+            self.dataAcc[:,:,self.currentFrameCount] = np.intc(self.Data.get())
+            self.currentFrameCount = self.currentFrameCount + 1
 
-        self.dataAcc[:,:,self.currentFrameCount] = np.intc(self.Data.get())
-        self.currentFrameCount = self.currentFrameCount + 1
     def cleanData(self):
         self.dataAcc = np.zeros((192,384,1000), dtype='int32')
         self.currentFrameCount = 0
