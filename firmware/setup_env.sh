46e710edd118ff4756e379a36eee819461a3c6e3
Dawood Alnajjar <dnajjar@slac.stanford.edu>
2023-10-11 04:05:57 -0700

    Fixed simulation issue after upgrading vivado do 2023. Got latest of epix-hr-leap-common

--- firmware/setup_env.sh
+++ firmware/setup_env.sh
@@ -2,6 +2,6 @@
 
 source /afs/slac.stanford.edu/g/reseng/xilinx/vivado_2023.1/Vivado/2023.1/settings64.sh
 # Setup the VCS software
-source  /afs/slac/g/reseng/vol33/synopsys/vcs/R-2020.12-1/settings.sh
+source  /afs/slac/g/reseng/vol37/synopsys/vcs/T-2022.06-SP2/settings.sh
 
 source /afs/slac.stanford.edu/g/reseng/zeromq/4.2.0/settings.sh
