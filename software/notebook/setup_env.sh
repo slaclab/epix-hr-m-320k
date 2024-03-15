ca9d0729df6ba6747741b13fc98ddef0deeba8f9
Dawood Alnajjar <dnajjar@slac.stanford.edu>
2024-03-15 13:59:44 -0700

    Generated a new conda environment for rogue 6 and jupyter

--- software/notebook/setup_env.sh
+++ software/notebook/setup_env.sh
@@ -7,5 +7,6 @@ source /afs/slac/g/reseng/rogue/anaconda_new_a/anaconda3/etc/profile.d/conda.sh
 ##################################
 # Activate Rogue conda Environment
 ##################################
-conda activate /afs/slac/g/controls/development/users/dnajjar/.conda/envs/ePixHrM
+conda activate /afs/slac/g/controls/development/users/dnajjar/.conda/envs/rogue6_jupyter
+
 
