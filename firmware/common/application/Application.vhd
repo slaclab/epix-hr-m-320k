d9adde1676b389d560759d449a9a4d2931ee383a
Dawood Alnajjar <dnajjar@slac.stanford.edu>
2024-03-23 06:15:23 -0700

    Timing event bus was generated as 8 byte, then propagated as 16 byte.

--- firmware/common/application/Application.vhd
+++ firmware/common/application/Application.vhd
@@ -653,7 +653,7 @@ begin
          TPD_G                 => TPD_G,
          SIMULATION_G          => SIMULATION_G,
          AXIL_CLK_FREQ_G       => AXIL_CLK_FREQ_C,
-         EVENT_AXIS_CONFIG_G   => PGP4_AXIS_CONFIG_C,
+         EVENT_AXIS_CONFIG_G   => SSI_CONFIG_INIT_C,
          NUM_EVENT_CHANNELS_G  => NUM_EVENT_CHANNELS_G,
          AXIL_BASE_ADDR_G      => XBAR_CONFIG_C(TIMING_INDEX_C).baseAddr
       )
