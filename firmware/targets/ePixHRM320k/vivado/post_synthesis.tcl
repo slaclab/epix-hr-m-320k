fbd2384fb41cb66f7afe2f921c52fe7355a97f13
Dawood Alnajjar <dnajjar@slac.stanford.edu>
2024-05-30 14:35:58 -0700

    Updated release register and submodules

--- firmware/targets/ePixHRM320k/vivado/post_synthesis.tcl
+++ firmware/targets/ePixHRM320k/vivado/post_synthesis.tcl
@@ -15,7 +15,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_env_var.tcl
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Bypass the debug chipscope generation
-#return
+return
 
 ############################
 ## Open the synthesis design
