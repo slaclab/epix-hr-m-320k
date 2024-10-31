121124bed92e1a83202339d46cf79df64f9d5dc4
Dawood Alnajjar <dnajjar@slac.stanford.edu>
2024-10-31 09:50:28 -0700

    Adding testbench for delayDetermination

--- firmware/targets/ePixHRM320k/ruckus.tcl
+++ firmware/targets/ePixHRM320k/ruckus.tcl
@@ -22,6 +22,7 @@ loadConstraints -dir  "$::DIR_PATH/hdl/"
 
 # Load local SIM source Code
 loadSource -sim_only -dir "$::DIR_PATH/tb"
-set_property top {ePixHRM320kTb} [get_filesets sim_1]
+#set_property top {ePixHRM320kTb} [get_filesets sim_1]
 #set_property top {chargeInjectionTb} [get_filesets sim_1]
+set_property top {DelayDeterminationGrpTb} [get_filesets sim_1]
 
